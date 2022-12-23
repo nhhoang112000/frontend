0c1b149ff75d12358bafe12c38d9e4486cb51d4f
Hoang <nguyenhuyhoang112k@gmail.com>
2022-12-23 10:22:08 +0700

    last fix

--- src/contexts/constants.js
+++ src/contexts/constants.js
@@ -1,7 +1,9 @@
 export const apiUrl =
 	process.env.NODE_ENV !== 'production'
 		? 'http://localhost:5000/api'
-		: 'https://backend-production-ec78.up.railway.app/api'
+		: 'http://localhost:5000/api'
+
+		//https://backend-production-ec78.up.railway.app/api
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'recourses'
 
